5e021a923299362ac8abb67e4cfd634ef938c017
Scraeling <scraeling@protonmail.com>
2020-12-11 09:01:09 +0530

    Update template to use the timer macro

--- _template/src/main.rs
+++ _template/src/main.rs
@@ -1,37 +1,26 @@
-use std::env;
 use std::fs::read_to_string;
-use std::time::Instant;
+use timer::time;
 
 fn main() {
-    let mut last: u128;
-    let mut now: u128;
-    let time = Instant::now();
+    let input = time!(parse_input);
+    let input_ref = input.as_ref();
+    let p1_answer = time!(part_1, input_ref);
+    let p2_answer = time!(part_2, input_ref);
 
-    let input = parse_input();
-    now = time.elapsed().as_micros();
-    println!("[{0}µ/{0}µ] Finished processing input", now);
-    last = now;
-
-    part_1();
-    now = time.elapsed().as_micros();
-    println!("[{}µ/{}µ] Finished Part 1", now - last, now);
-    last = now;
-
-    part_2();
-    now = time.elapsed().as_micros();
-    println!("[{}µ/{}µ] Finished Part 2", now - last, now);
+    println!("{}", p1_answer);
+    println!("{}", p2_answer);
 }
 
 fn parse_input() -> String {
-    read_to_string(&env::args().collect::<Vec<String>>()[1]).unwrap()
+    read_to_string("input.txt").unwrap()
 }
 
-fn part_1() {
-
+fn part_1(input: &str) -> String {
+    "".to_string()
 }
 
-fn part_2() {
-
+fn part_2(input: &str) -> String {
+    "".to_string()
 }
 
 #[cfg(test)]
