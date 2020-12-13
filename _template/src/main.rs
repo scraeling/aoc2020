757f382766deb7c6abd6d2414e3bf02a04c058c1
Scraeling <scraeling@protonmail.com>
2020-12-13 09:04:09 +0530

    Update template

--- _template/src/main.rs
+++ _template/src/main.rs
@@ -1,24 +1,24 @@
 use std::fs::read_to_string;
 use timer::time;
 
-fn main() {
-    let input = read_to_string("input.txt").unwrap();
-    let parsed_input = time!(parse_input(input));
-    let p1_answer = time!(part_1());
-    let p2_answer = time!(part_2());
-
-    println!("{}", p1_answer);
-    println!("{}", p2_answer);
-}
-
 fn parse_input(input: String) {
 
 }
 
-fn part_1() {
-
+fn part_1() -> usize {
+    0
 }
 
-fn part_2() {
+// fn part_2() {
 
+// }
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    let parsed_input = time!(parse_input(input));
+    let p1_answer = time!(part_1());
+    // let p2_answer = time!(part_2());
+
+    println!("{}", p1_answer);
+    //println!("{}", p2_answer);
 }
